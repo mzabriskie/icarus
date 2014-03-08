f4e568526d9742b1fc2e0ddff7549bd85e7816c3
Matt Zabriskie <mzabriskie@gmail.com>
2014-03-08 13:53:38 -0700

    Adding npm and grunt config

---
+++ Gruntfile.js
@@ -0,0 +1,15 @@
+module.exports = function (grunt) {
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-nodeunit');
+
+	grunt.initConfig({
+		jshint: {
+			all: ['Gruntfile.js', 'index.js']
+		},
+		nodeunit: {
+			all: ['test/*.js']
+		}
+	});
+
+	grunt.registerTask('default', ['jshint', 'nodeunit']);
+};
