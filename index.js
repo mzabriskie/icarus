abf323d74262335c19e4e9d23f738d9b517066b5
Matt Zabriskie <mzabriskie@gmail.com>
2014-03-09 21:00:53 -0600

    Moving logic from index into relevant file

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-var express = require('express'),
-    app = express(),
-    port = 3000;
-
-
-app.get('/:module/:version', function (req, res) {
-	res.send('Module: ' + req.params.module + ' <br/>Version: ' + req.params.version);
-});
-
-app.listen(port);
+module.exports = require('./lib/icarus');
