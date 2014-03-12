33e5f42b455d637bb084117a0f8d0ab07f33c3aa
Matt Zabriskie <mzabriskie@gmail.com>
2014-03-11 23:56:39 -0600

    Adding minimist to handle options

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-module.exports = require('./lib/icarus');
+var icarus = require('./lib/icarus'),
+	argv = require('minimist')(process.argv.slice(2));
+
+// Clean the cache
+if (argv._.indexOf('clean') > -1) {
+	icarus.clean();
+}
+
+// Warm the cache
+if (argv._.indexOf('cache') > -1) {
+	icarus.cache();
+}
+
+// Serve the goods
+if (argv._.indexOf('serve') > -1) {
+	icarus.serve(argv.p);
+}
